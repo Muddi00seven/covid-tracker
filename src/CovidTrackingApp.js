c0789b36e23c31695d41ae1dd754ac7627c046f8
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-28 14:15:31 +0500

    Build 0.0.2
    Structure Added

---
+++ src/CovidTrackingApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card , Chart , CountryPicker} from './Components';
+
+const CovidTrackingApp = () => {
+    return (
+        <div>
+<Card/> <Chart/> <CountryPicker/>
+
+        </div>
+    )
+}
+
+export default CovidTrackingApp;
