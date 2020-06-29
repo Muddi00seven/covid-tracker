8c14521383ba10c16538abf9c9f6d3034e677f68
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-29 12:51:50 +0500

    Update Cards.jsx

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 // import CountUp from 'countup';
-import { styles } from './Cards.module.css'
+// import { styles } from './Cards.module.css'
 const Cards = ( {data : {confirmed , recovered , deaths  } }) => {
     if(!confirmed) {
       return 'loading'
