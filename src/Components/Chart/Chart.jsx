5422e8546a3e15081613ad531b346d1d8a82d3fd
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-30 20:58:22 +0500

    build 0.1.2
    COMPLETE BUT NEED FEW MORE UI CHANGES

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,9 +1,9 @@
 import React, {useState , useEffect} from 'react';
 import { fetchDailyData} from '../../api';
-import { Line } from 'react-chartjs-2';
+import { Line , Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css'
 
-const Chart = () => {
+const Chart = ({data: { confirmed , recovered , deaths} , country}) => {
     const [dailyData , setDailyData] = useState([]);
     useEffect(() => {
         const fetchAPI = async () => {
@@ -12,7 +12,7 @@ const Chart = () => {
     }
         fetchAPI()
     
-    });
+    } , []) ;
 
     const lineChart = (
         dailyData[0]
@@ -36,10 +36,32 @@ const Chart = () => {
             }}
         /> ) :null  
         );
+
+        const barChart = (
+            confirmed 
+            ? (<Bar
+            data = {{
+                labels : ['Infected' , 'Recovered' , 'Deaths'],
+                datasets : [{
+                    label : 'people',
+                    backgroundColor: [' rgba(14, 36, 134, 0.801)' , 
+                    'rgba(7, 131, 59, 0.726)' ,
+                    'rgba(138, 5, 5, 0.753)'],
+                        data:[confirmed.value, recovered.value ,deaths.value]
+                }]
+
+            }}
+
+            options={{
+                legemd: {display: false},
+                title : {display : true , text : `Current state in ${country}`}
+            }}
+            /> ) :null
+        )
     return (
         <div className={styles.container}>
-            {lineChart}
-        </div>
+                {country ? barChart : lineChart}
+            </div>
     )
 }
 
--- src/CovidTrackingApp.js
+++ src/CovidTrackingApp.js
@@ -15,17 +15,17 @@ class CovidTrackingApp extends React.Component {
     
       handleCountryChange = async (country) => {
             const fetchedData = await fetchData(country);
-this.setState({data : fetchedData });
+this.setState({data : fetchedData , country : country});
     }
     render() {
-        const { data } = this.state;
+        const { data , country } = this.state;
 return (
 
     <div className={styles.container}>
      <Card data={data}/> 
      <CountryPicker handleCountryChange={this.handleCountryChange}/>
 
-     <Chart/> 
+     <Chart data={data} country={country}/> 
   </div>
 )
 
